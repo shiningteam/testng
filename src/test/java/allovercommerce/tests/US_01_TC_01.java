2a0e6d6454e0529ea20ed51ce6d2a8024f1e5bee
Merve Özcan <ozcanmerve.tr@gmail.com>
2023-02-03 23:06:40 +0300

    resolved conflict

--- src/test/java/allovercommerce/tests/US_01_TC_01.java
+++ src/test/java/allovercommerce/tests/US_01_TC_01.java
@@ -101,15 +101,7 @@ public class US_01_TC_01 {
         String errorMessage=customerHomePage.errorMessage.getText();
         System.out.println(errorMessage);
         Assert.assertEquals(errorMessage,"Please enter a valid account username.");
-
-
-        //ReusableMethods.getScreenshot("negativeLoginScreenshot2");
-
-//        ReusableMethods.getScreenshot("negativeLoginScreenshot2");
-
-
-//        ReusableMethods.getScreenshot("negativeLoginScreenshot2");
-
+       ReusableMethods.getScreenshot("negativeLoginScreenshot2");
         }
     @Test //wrong email bug
     public void testcase_04() throws IOException {
@@ -140,16 +132,7 @@ public class US_01_TC_01 {
         String errorMessage=customerHomePage.errorMessage.getText();
         System.out.println(errorMessage);
         Assert.assertFalse(false,"Please enter a valid email address.");
-
-
-        //ReusableMethods.getScreenshot("negativeLoginScreenshot3");
-
-//        ReusableMethods.getScreenshot("negativeLoginScreenshot3");
-
-
-//        ReusableMethods.getScreenshot("negativeLoginScreenshot3");
-
-
+        ReusableMethods.getScreenshot("negativeLoginScreenshot3");
     }
     @Test //wrong password bug
     public void testcase_05() throws IOException {
